69bf7758069c53d13d62e8112eed36bea03a12b2
ScottGrizzell <scott.grizzell@oit.edu>
2025-02-02 22:53:00 -0800

    Broken code, WIP commit

--- lib/friends_list.dart
+++ lib/friends_list.dart
@@ -16,23 +16,89 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 ///*********************************
-/// Name: SocialMediaPage
+/// Name: FriendsList
 /// 
 /// Description: Root stateless widget of 
-/// the SocialMediaPage, builds and displays 
+/// the FriendsList, builds and displays 
 /// social media page view
 ///*********************************
-class FriendsList extends StatelessWidget {
+class FriendsList extends StatefulWidget {
   const FriendsList({super.key});
+
+  @override
+  _FriendsListState createState() => _FriendsListState();
+
+}
+
+class _FriendsListState extends State<FriendsList>{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final TextEditingController _searchController = TextEditingController();
+  List<String> _friendsList = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // TODO ADD CHECK FOR IF FRIEND HAS ALREADY BEEN ADDED
+  // TODO FIGURE OUT WHY FRIEND DOC ISN'T BEING VALIDATED
+  void _addFriend(String friendUID) async {
+  DocumentSnapshot friendDocRef = await _firestore.collection('UID').doc(friendUID).get();
+  print('FriendUID: ${friendUID}');
+  print('Friend Document Exists: ${friendDocRef.exists}');
+  print('Friend Document as string?: ${friendDocRef.data()}');
+
+    if (friendDocRef.exists) {
+         DocumentReference userDocRef = _firestore.collection('UID').doc(_auth.currentUser?.uid);
+         // Adding friend UID to friends field in UID document
+         await userDocRef.set({
+          'friends': FieldValue.arrayUnion([friendUID])
+         }, SetOptions(merge: true));
+        
+         
+        await userDocRef.collection('friends').doc(friendUID).set({
+            'UID': friendUID
+        });
+        
+        ScaffoldMessenger.of(context).showSnackBar(
+         const SnackBar(content: Text('Friend added!'))
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('User not found')),
+        );
+      }
+    }
+  
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: const Text("Friends List"),
+        title: const Text("Friends List"),
       ),
-    body: const Center(
-      child: Text("Friend Icons")
-    )
+      body: Column(
+      children: [
+        
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child:TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              labelText: 'Enter Friend UID',
+              suffixIcon: IconButton(
+              icon: const Icon(Icons.search),
+               onPressed: () => _addFriend(_searchController.text),
+              ),
+            ),
+          )
+        )
+
+       ],
+      )
     );
   }
 }
