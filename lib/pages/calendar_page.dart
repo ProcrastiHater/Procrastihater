d076898a4f33af4bdbc059f76c0a254135d85fa7
adipat542 <aditya.dwivedi.pathak@gmail.com>
2025-02-21 12:54:11 -0800

    Selected dates now apppear to the user

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -10,11 +10,19 @@ library;
 //Dart Imports
 import 'package:flutter/material.dart';
 
-//Other Imports
+//Calendar Imports
 import 'package:omni_datetime_picker/omni_datetime_picker.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime? dateTime;
+  List<DateTime>? dateTimeRange;
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +33,36 @@ class CalendarPage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () async {
-                final List<DateTime>? dateTime =
-                    await showOmniDateTimeRangePicker(context: context);
-
+                final result = await showOmniDateTimePicker(context: context);
+                setState(() {
+                  dateTime = result;
+                });
                 // Use dateTime here
+                debugPrint('dateTime: $dateTime');
+              },
+              child: const Text('Show DateTime Picker'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final result =
+                    await showOmniDateTimeRangePicker(context: context);
+                setState(() {
+                  dateTimeRange = result;
+                });
                 debugPrint('dateTime: $dateTime');
               },
               child: const Text('Open Calendar'),
             ),
+            if (dateTime != null)
+              Text(
+                'Selected Date: ${dateTime!.toString()}',
+                style: const TextStyle(fontSize: 16),
+              ),
+            if (dateTimeRange != null)
+              Text(
+                'Selected Range: ${dateTimeRange!.map((date) => date.toString()).join(" to ")}',
+                style: const TextStyle(fontSize: 16),
+              ),
           ],
         ),
       ),
