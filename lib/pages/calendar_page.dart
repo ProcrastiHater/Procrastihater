0c2f96c835a4ce230ddb2cd7109169c93af5b620
adipat542 <aditya.dwivedi.pathak@gmail.com>
2025-02-24 13:02:00 -0800

    Able to add an event to google calendar off a singular datetime selection

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 
 //Calendar Imports
 import 'package:omni_datetime_picker/omni_datetime_picker.dart';
+import 'package:add_2_calendar/add_2_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -24,6 +25,21 @@ class _CalendarPageState extends State<CalendarPage> {
   DateTime? dateTime;
   List<DateTime>? dateTimeRange;
 
+  Event buildEvent({Recurrence? recurrence}) {
+    return Event(
+      title: 'Test event',
+      description: 'example',
+      location: 'Flutter app',
+      startDate: dateTime ?? DateTime.now(),
+      endDate: (dateTime ?? DateTime.now()).add(const Duration(minutes: 30)),
+      allDay: false,
+      androidParams: const AndroidParams(
+        emailInvites: ["test@example.com"],
+      ),
+      recurrence: recurrence,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,19 +49,19 @@ class _CalendarPageState extends State<CalendarPage> {
           children: [
             ElevatedButton(
               onPressed: () async {
-                final result = await showOmniDateTimePicker(context: context);
+                final result = await showOmniDateTimePicker(
+                    context: context, is24HourMode: false);
                 setState(() {
                   dateTime = result;
                 });
-                // Use dateTime here
                 debugPrint('dateTime: $dateTime');
               },
               child: const Text('Show DateTime Picker'),
             ),
             ElevatedButton(
               onPressed: () async {
-                final result =
-                    await showOmniDateTimeRangePicker(context: context);
+                final result = await showOmniDateTimeRangePicker(
+                    context: context, is24HourMode: false);
                 setState(() {
                   dateTimeRange = result;
                 });
@@ -65,6 +81,15 @@ class _CalendarPageState extends State<CalendarPage> {
                 style: const TextStyle(fontSize: 16),
                 textAlign: TextAlign.center,
               ),
+            ListTile(
+              title: const Text('Add normal event'),
+              trailing: const Icon(Icons.calendar_today),
+              onTap: () {
+                Add2Calendar.addEvent2Cal(
+                  buildEvent(),
+                );
+              },
+            ),
           ],
         ),
       ),
