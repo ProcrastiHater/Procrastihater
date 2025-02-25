50c07d2a8c076bdb9bc9b1d55397be12589fd014
ScottGrizzell <scott.grizzell@oit.edu>
2025-02-25 09:54:43 -0800

    Working timer to count up

--- lib/study_mode.dart
+++ lib/study_mode.dart
@@ -11,17 +11,38 @@ class StudyModePage extends StatefulWidget {
 }
 
 class StudyModePageState extends State<StudyModePage> {
-
+  Timer? _timer;
+  int _elapsedSeconds = 0;
 
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+void _startStudySession() {
+    _elapsedSeconds = 0;
+    
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _elapsedSeconds++;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Study Mode")),
-
+      body: Column(
+        children: [
+         ElevatedButton(
+              onPressed: _startStudySession,
+              child: const Text("Begin Productive Session"),
+            ),
+      Text(
+              "Time Elapsed: ${(_elapsedSeconds ~/ 60).toString().padLeft(2, '0')}:${(_elapsedSeconds % 60).toString().padLeft(2, '0')}",
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+        ]
+      )
     );
   }
 }
