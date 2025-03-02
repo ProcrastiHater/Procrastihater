977d828ceaaa416fe5f803b480c23a2c7ed6634b
adipat542 <aditya.dwivedi.pathak@gmail.com>
2025-03-01 22:22:10 -0800

    Merged calendar pages, still not functional

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,7 +1,9 @@
 ///*********************************
-/// Name: calendar.dart
+/// Name: calendar_page.dart
 ///
-/// Description: 
+/// Description: Allows the user to
+/// specify dates and times to be
+/// blocked off in their device calendar
 ///*******************************
 library;
 
@@ -10,20 +12,191 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-///*********************************
-/// Name: CalendarPage
-/// 
-/// Description:
-///*********************************
-class CalendarPage extends StatelessWidget {
+//Calendar Imports
+import 'package:omni_datetime_picker/omni_datetime_picker.dart';
+import 'package:add_2_calendar/add_2_calendar.dart';
+
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime? dateTime;
+  List<DateTime>? dateTimeRange;
+  String? title;
+  String? description;
+  String? location;
+  String? reccurance;
+  bool? isAllDay;
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+
+  final List<Map<String, dynamic>> recurrenceOptions = [
+    {'label': 'None', 'value': null},
+    {'label': 'Weekly', 'value': Frequency.weekly},
+    {'label': 'Monthly', 'value': Frequency.monthly},
+    {'label': 'Yearly', 'value': Frequency.yearly},
+  ];
+
+  Recurrence? getRecurrenceFromSelection() {
+    for (var option in recurrenceOptions) {
+      if (option['label'] == reccurance) {
+        Frequency? frequency = option['value'];
+        return frequency != null
+            ? Recurrence(frequency: frequency, interval: 1, ocurrences: 1)
+            : null;
+      }
+    }
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController.addListener(() {
+      setState(() {
+        title = _titleController.text;
+      });
+    });
+    _descriptionController.addListener(() {
+      setState(() {
+        description = _descriptionController.text;
+      });
+    });
+    _locationController.addListener(() {
+      setState(() {
+        location = _locationController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _locationController.dispose();
+    super.dispose();
+  }
+
+  Event buildEvent({Recurrence? recurrence}) {
+    return Event(
+      title: title ?? 'Test event',
+      description: description ?? 'example',
+      location: location ?? 'Flutter app',
+      startDate: dateTime ?? DateTime.now(),
+      endDate: (dateTime ?? DateTime.now()).add(const Duration(minutes: 30)),
+      allDay: isAllDay ?? false,
+      recurrence: recurrence,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Calendar"),
+      appBar: AppBar(title: const Text('ProcrastiPlanner')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(
+                labelText: 'Event Title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _descriptionController,
+              decoration: const InputDecoration(
+                labelText: 'Event Description',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _locationController,
+              decoration: const InputDecoration(
+                labelText: 'Event Location',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await showOmniDateTimePicker(
+                    context: context, is24HourMode: true);
+                setState(() {
+                  dateTime = result;
+                });
+                debugPrint('dateTime: $dateTime');
+              },
+              child: const Text('Select Event Time'),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Checkbox(
+                  value: isAllDay,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isAllDay = value ?? false;
+                    });
+                  },
+                ),
+                const Text('All Day Event'),
+                const Spacer(),
+                const Text('Repeats: '),
+                const SizedBox(width: 8),
+                DropdownButton<String>(
+                  value: reccurance,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      reccurance = newValue;
+                    });
+                  },
+                  items:
+                      recurrenceOptions.map<DropdownMenuItem<String>>((option) {
+                    return DropdownMenuItem<String>(
+                      value: option['label'],
+                      child: Text(option['label']),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await showOmniDateTimeRangePicker(
+                    context: context, is24HourMode: false);
+                setState(() {
+                  dateTimeRange = result;
+                });
+                debugPrint('dateTime: $dateTime');
+              },
+              child: const Text('Open Calendar'),
+            ),
+            const SizedBox(height: 16),
+            ListTile(
+              title: const Text('Add event to calendar'),
+              trailing: const Icon(Icons.calendar_today),
+              onTap: () {
+                Add2Calendar.addEvent2Cal(
+                  buildEvent(recurrence: getRecurrenceFromSelection()),
+                );
+              },
+            ),
+          ],
         ),
-        body: Center(child: Text("Calendar")),
-      );
+      ),
+    );
   }
 }
