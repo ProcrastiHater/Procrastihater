efa92426e66bda7f69672ede64788727b726a7f6
Bridget Jeppesen <bridget.jeppesen@oit.edu>
2025-03-03 21:06:14 -0800

    Added navigation to AppLimitsPage; Added ui outline for app limit usage

--- lib/pages/app_limits_page.dart
+++ lib/pages/app_limits_page.dart
@@ -7,12 +7,20 @@
 ///*********************************
 library;
 
+import 'package:app_screen_time/apps_list.dart';
 import 'package:app_screen_time/main.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
 
+///*********************************
+/// Name: AppLimitsPage
+/// 
+/// Description: Root stateful widget of 
+/// the AppLimitsPage, builds and displays 
+/// app limits page view
+///*********************************
 class AppLimitsPage extends StatefulWidget{
   const AppLimitsPage({super.key});
 
@@ -21,6 +29,7 @@ class AppLimitsPage extends StatefulWidget{
 }
 
 class AppLimitsPageState extends State<AppLimitsPage>{
+  final TextEditingController _appLimitController = TextEditingController();
   @override
   void initState() {
     super.initState();
@@ -28,6 +37,27 @@ class AppLimitsPageState extends State<AppLimitsPage>{
 
   @override
   Widget build(BuildContext context) {
-    return Text("Placeholder");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Screen Time Limits"),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemCount: appNames.length,
+        itemBuilder: (context, index){
+          return ListTile(
+              leading: Text('App Icon?'),
+              title: Text(appNames[index]),
+              trailing: SizedBox(
+                width: 100,
+                child: TextField(
+                  controller: _appLimitController,
+                  decoration: InputDecoration(labelText: 'Time limit')
+                )
+              )
+          );
+        }
+      )
+    );
   }
 }
