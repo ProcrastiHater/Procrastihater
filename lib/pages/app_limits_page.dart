b0aa106c2c7887ce9ede7adc28341f4941de69bd
Bridget Jeppesen <bridget.jeppesen@oit.edu>
2025-03-04 15:39:32 -0800

    Added save button and DB communications

--- lib/pages/app_limits_page.dart
+++ lib/pages/app_limits_page.dart
@@ -47,37 +47,79 @@ class AppLimitsPageState extends State<AppLimitsPage>{
       body: ListView.builder(
         padding: EdgeInsets.zero,
         itemCount: appNames.length,
-        itemBuilder: (context, index){
+        itemBuilder: (context, index) {
           return ListTile(
-              tileColor: Colors.indigo.shade100,
-              //leading: Text('App Icon?'),
-              title: Text(
-                appNames[index],
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: appNameToColor[appNames[index]],
-                ),
+            contentPadding: EdgeInsets.only(bottom: 5, left: 10, right: 10),
+            tileColor: Colors.indigo.shade100,
+            //leading: Text('App Icon?'),
+            title: Text(
+              appNames[index],
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: appNameToColor[appNames[index]],
               ),
-              trailing: SizedBox(
-                width: 100,
-                child: TextField(
-                  controller: _appLimitControllers[index],
-                  decoration: InputDecoration(
-                    labelText: 'Time limit',
+            ),
+            trailing: SizedBox(
+              width: 150,
+              child: TextField(
+                controller: _appLimitControllers[index],
+                decoration: InputDecoration(
+                  labelText: 'Time limit',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.save
+                    ),
+                    onPressed: () => _updateAppLimit(appNames[index], _appLimitControllers[index].text),
                   )
                 )
               )
+            )
           );
         }
       )
     );
   }
+
+  Future<void> _updateAppLimit(String appName, String newLimitStr) async {
+    updateUserRef();
+    int? newLimit = int.tryParse(newLimitStr);
+    if (newLimit == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Entered limit is not a number'))
+      );
+    }
+    else{
+      var limitRef = userRef.collection('limits').doc(appName);
+      if (newLimit < 0){
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Limit must be greater than or equal to 0'))
+        );
+      }
+      else if (newLimit == 0){
+        try {
+          var limitDoc = await limitRef.get();
+          //Delete limit if it already exists
+          if (limitDoc.exists) {
+            limitRef.delete();
+          } else {
+            //Print "Limit doesn't exist yet" or something
+          }
+        }
+        catch(e){
+          debugPrint("Error deleting limit: $e");
+        }
+      }
+      else{
+        try {
+          //Set Limit in DB
+          await limitRef.set(
+              {'limit': ((newLimit / 60) * 100).round().toDouble() / 100},
+              SetOptions(merge: true));
+        }
+        catch(e){
+          debugPrint("Error setting limit: $e");
+        }
+      }
+    }
+  }
 }
-
-//Future<void> _updateAppLimit(String appName, int newLimit)
-//{
-  //updateUserRef();
-  //if(newLimit == 0){
-
-  //}
-//}
