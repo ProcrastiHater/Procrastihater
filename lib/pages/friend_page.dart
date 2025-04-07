ab425728743ac275032546744767aa576be4a538
Anthony Jewell <ajjewell2004@gmail.com>
2025-04-07 08:49:10 -0700

    drawer update

--- lib/pages/friend_page.dart
+++ lib/pages/friend_page.dart
@@ -16,6 +16,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+//Page Imports 
+import '/main.dart';
+
 ///*********************************
 /// Name: HistoricalDataPage
 /// 
@@ -38,10 +41,6 @@ class FriendsPage extends StatelessWidget {
         }
       },
       child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: const Text("ProcrastiFriends"),
-        ),
         body: FriendsList(),
       ),
     );
@@ -116,6 +115,74 @@ void _pokeFriend(String friendUID) async {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+        title: Text("ProcrastiStats"),
+        actions: [
+          // Creating little user icon you can press to view account info
+          IconButton(
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(
+                // Use user's pfp as icon image if there is no pfp use this link as a default
+                auth.currentUser?.photoURL ?? 'https://picsum.photos/id/237/200/300',
+              ),
+            ),
+            onPressed: () async {
+              await Navigator.pushNamed(context, "/profileSettings");
+              // Reload the user in case anything changed
+              await auth.currentUser?.reload();
+              // Reload UI in case things changed
+              setState(() {});
+            },
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+              child:  DrawerHeader(
+              decoration: BoxDecoration(
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0),
+                    child: Image.asset("assets/logo.jpg"),
+                  ),
+                  Text("ProcrastiTools",),
+              ],
+              )
+              ),
+            ),
+              ListTile(
+              trailing: Icon(Icons.calendar_today),
+              title: Text("Calendar"),
+              onTap:() {
+                Navigator.pushNamed(context, '/calendarPage');
+              },
+            ),
+           // const Divider(),
+             ListTile(
+              trailing: Icon(Icons.school),
+              title: Text("Study Mode"),
+              onTap: () {
+                Navigator.pushNamed(context, '/studyModePage');
+              },
+            ),
+            //const Divider(),
+            ListTile(
+              trailing: Icon(Icons.alarm),
+              title: Text("App Limits"),
+              onTap: () {
+                Navigator.pushNamed(context, '/appLimitsPage');
+              },
+            )
+          ],
+        ),
+      ),
       body: Column(
       children: [
         Padding(
