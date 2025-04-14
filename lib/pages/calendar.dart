9d7b05f3205f09e7e2a026781d5006136f3e2898
adipat542 <aditya.dwivedi.pathak@gmail.com>
2025-04-13 20:57:16 -0700

    Rounded ListTile, removed colorscheme

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -34,18 +34,6 @@ class CalendarPage extends StatefulWidget {
 /// Description: State for the CalendarPage
 ///*********************************
 class _CalendarPageState extends State<CalendarPage> {
-  final ColorScheme _colorScheme = const ColorScheme(
-    primary: Color.fromARGB(255, 10, 27, 46),
-    secondary: Color.fromARGB(255, 19, 52, 88),
-    surface: Color.fromARGB(255, 10, 27, 46),
-    error: Color(0xFFFF5252),
-    onPrimary: Color.fromARGB(255, 19, 52, 88),
-    onSecondary: Color.fromARGB(255, 19, 60, 88),
-    onSurface: Color.fromARGB(255, 252, 231, 193),
-    onError: Color.fromARGB(255, 10, 27, 46),
-    brightness: Brightness.light,
-  );
-
   List<DateTime>? dateTimeRange;
   String? title;
   String? description;
@@ -77,6 +65,24 @@ class _CalendarPageState extends State<CalendarPage> {
     });
   }
 
+  ///*********************************
+  /// Name: getColor
+  ///
+  /// Description: gets and sets the color of the
+  /// all day checkbox based off the interaction state
+  ///*********************************
+  Color getColor(Set<WidgetState> states) {
+    const Set<WidgetState> interactiveStates = <WidgetState>{
+      WidgetState.pressed,
+      WidgetState.hovered,
+      WidgetState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.grey;
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -88,7 +94,8 @@ class _CalendarPageState extends State<CalendarPage> {
   ///*********************************
   /// Name: buildEvent
   ///
-  /// Description: Creates the event to be added to device calendar
+  /// Description: Creates an event to be
+  /// added to device calendar
   ///*********************************
   Event buildEvent({Recurrence? recurrence}) {
     Recurrence? recurrence;
@@ -130,14 +137,7 @@ class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'ProcrastiPlanner',
-          style: TextStyle(color: _colorScheme.onSurface),
-        ),
-        backgroundColor: _colorScheme.primary,
-      ),
-      backgroundColor: _colorScheme.primary,
+      appBar: AppBar(title: const Text('ProcrastiPlanner')),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -145,107 +145,53 @@ class _CalendarPageState extends State<CalendarPage> {
           children: [
             TextField(
               controller: _titleController,
-              style: TextStyle(color: _colorScheme.onSurface),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Event Title',
-                labelStyle: TextStyle(color: _colorScheme.onSurface),
                 border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: _colorScheme.onSurface, width: 2.0),
-                ),
               ),
             ),
             const SizedBox(height: 16),
             TextField(
               controller: _descriptionController,
-              style: TextStyle(color: _colorScheme.onSurface),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Event Description',
-                labelStyle: TextStyle(color: _colorScheme.onSurface),
                 border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: _colorScheme.onSurface, width: 2.0),
-                ),
               ),
               maxLines: 3,
             ),
             const SizedBox(height: 16),
             TextField(
               controller: _locationController,
-              style: TextStyle(color: _colorScheme.onSurface),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Event Location',
-                labelStyle: TextStyle(color: _colorScheme.onSurface),
                 border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: _colorScheme.onSurface, width: 2.0),
-                ),
               ),
             ),
             ListTile(
-              title: Text(
-                'All Day',
-                style: TextStyle(color: _colorScheme.onSurface),
-              ),
-              trailing: Checkbox(
-                checkColor: _colorScheme.primary,
-                fillColor: WidgetStateProperty.resolveWith((states) {
-                  if (states.contains(WidgetState.selected)) {
-                    return _colorScheme.onSurface;
-                  }
-                  return _colorScheme.onSecondary;
-                }),
-                value: isAllDay,
-                onChanged: (bool? value) {
-                  setState(() {
-                    isAllDay = value!;
-                  });
-                },
-              ),
-            ),
+                title: const Text('All Day'),
+                trailing: Checkbox(
+                  checkColor: Colors.white,
+                  fillColor: WidgetStateProperty.resolveWith(getColor),
+                  value: isAllDay,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isAllDay = value!;
+                    });
+                  },
+                )),
             const SizedBox(height: 16),
             DropdownButtonFormField<String>(
-              dropdownColor: _colorScheme.primary,
-              style: TextStyle(color: _colorScheme.onSurface),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Recurrence',
-                labelStyle: TextStyle(color: _colorScheme.onSurface),
                 border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: _colorScheme.onSurface, width: 2.0),
-                ),
               ),
               value: recurrenceType,
-              items: [
-                DropdownMenuItem(
-                  value: null,
-                  child: Text('No Recurrence',
-                      style: TextStyle(color: _colorScheme.onSurface)),
-                ),
-                DropdownMenuItem(
-                  value: 'daily',
-                  child: Text('Daily',
-                      style: TextStyle(color: _colorScheme.onSurface)),
-                ),
-                DropdownMenuItem(
-                  value: 'weekly',
-                  child: Text('Weekly',
-                      style: TextStyle(color: _colorScheme.onSurface)),
-                ),
-                DropdownMenuItem(
-                  value: 'monthly',
-                  child: Text('Monthly',
-                      style: TextStyle(color: _colorScheme.onSurface)),
-                ),
-                DropdownMenuItem(
-                  value: 'yearly',
-                  child: Text('Yearly',
-                      style: TextStyle(color: _colorScheme.onSurface)),
-                ),
+              items: const [
+                DropdownMenuItem(value: null, child: Text('No Recurrence')),
+                DropdownMenuItem(value: 'daily', child: Text('Daily')),
+                DropdownMenuItem(value: 'weekly', child: Text('Weekly')),
+                DropdownMenuItem(value: 'monthly', child: Text('Monthly')),
+                DropdownMenuItem(value: 'yearly', child: Text('Yearly')),
               ],
               onChanged: (String? value) {
                 setState(() {
@@ -257,47 +203,25 @@ class _CalendarPageState extends State<CalendarPage> {
             ElevatedButton(
               onPressed: () async {
                 final result = await showOmniDateTimeRangePicker(
-                  context: context,
-                  is24HourMode: true,
-                );
+                    context: context, is24HourMode: true);
                 setState(() {
                   dateTimeRange = result;
                 });
                 debugPrint('dateTime: $dateTimeRange');
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: _colorScheme.onSecondary,
-                foregroundColor: _colorScheme.onSurface,
-              ),
-              child: Text('Event Times',
-                  style: TextStyle(color: _colorScheme.onSurface)),
+              child: const Text('Event Times'),
             ),
             const SizedBox(height: 24),
-            Card(
-              color: _colorScheme.onSecondary,
-              elevation: 3,
+            ListTile(
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25),
-                  ),
-                ),
-                title: Text(
-                  'Add event to calendar',
-                  style: TextStyle(color: _colorScheme.onSurface),
-                ),
-                trailing:
-                    Icon(Icons.calendar_today, color: _colorScheme.onSurface),
-                tileColor: _colorScheme.onSecondary,
-                onTap: () {
-                  Add2Calendar.addEvent2Cal(
-                    buildEvent(),
-                  );
-                },
-              ),
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              title: const Text('Add event to calendar'),
+              trailing: const Icon(Icons.calendar_today),
+              onTap: () {
+                Add2Calendar.addEvent2Cal(
+                  buildEvent(),
+                );
+              },
             ),
           ],
         ),
