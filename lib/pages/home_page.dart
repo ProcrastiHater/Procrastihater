a9fe563bc3d670e210e82b775f941b76ee8ee451
adipat542 <aditya.dwivedi.pathak@gmail.com>
2025-05-05 11:52:28 -0700

    Exit confimation working on emulator, still WIP for getting functionality without old methods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -116,48 +116,27 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  ///*********************************************************
-  /// Name: didChangeAppLifecycleState
-  ///
-  /// Description: Monitors the app's widget to detect if it has been
-  /// paused or force closed. If detected it displays a warning to the
-  /// user to ensure they want to exit the app
-  ///*********************************************************
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.paused ||
-        state == AppLifecycleState.detached) {
-      showAlertOnPause = true;
-      _showExitWarningDialog();
-    } else if (state == AppLifecycleState.resumed || !showAlertOnPause) {
-      showAlertOnPause = false;
-    }
-  }
+  //TODO: see if WillPopScope gives the neccesary functionality
 
-  void _showExitWarningDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Warning'),
-          content:
-              Text('You are about to exit the app. Do you want to continue?'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Exit'),
-              onPressed: () {
-                SystemNavigator.pop(); // This will close the app
-              },
-            ),
-            TextButton(
-              child: Text('Continue'),
-              onPressed: () {
-                Navigator.of(context).pop(); // Dismiss the dialog
-              },
-            ),
-          ],
-        );
-      },
-    );
+  Future<bool> _showExitConfirmationDialog() async {
+    return await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Exit App'),
+            content: Text('Are you sure you want to exit the app?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
   }
 
   @override
@@ -165,219 +144,233 @@ class _MyHomePageState extends State<MyHomePage> {
     //Screensize
     double? screenWidth = MediaQuery.of(context).size.width;
     double? screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ProcrastiStats"),
-        actions: [
-          // Creating little user icon you can press to view account info
-          IconButton(
-            icon: CircleAvatar(
-              backgroundImage: NetworkImage(
-                // Use user's pfp as icon image if there is no pfp use this link as a default
-                auth.currentUser?.photoURL ??
-                    'https://picsum.photos/id/237/200/300',
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        if (didPop) return;
+
+        final bool shouldPop = await _showExitConfirmationDialog();
+
+        // If user confirmed exit, close the app
+        if (shouldPop && context.mounted) {
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("ProcrastiStats"),
+          actions: [
+            // Creating little user icon you can press to view account info
+            IconButton(
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  // Use user's pfp as icon image if there is no pfp use this link as a default
+                  auth.currentUser?.photoURL ??
+                      'https://picsum.photos/id/237/200/300',
+                ),
               ),
-            ),
-            onPressed: () async {
-              await Navigator.pushNamed(context, "/profileSettings");
-              // Reload the user in case anything changed
-              await auth.currentUser?.reload();
-              // Reload UI in case things changed
-              setState(() {});
-            },
-          )
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.all(4.0),
-          children: <Widget>[
-            SizedBox(
-              height: screenHeight * .15,
-              child: DrawerHeader(
-                  child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "ProcrastiTools",
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12.0),
-                    child: Image.asset("assets/logo.jpg"),
-                  ),
-                ],
-              )),
-            ),
-            ListTile(
-              trailing: Icon(Icons.calendar_today),
-              title: Text("Calendar"),
-              onTap: () {
-                Navigator.pushNamed(context, '/calendarPage');
-              },
-            ),
-            const Divider(
-              height: 1,
-              color: lightBeige,
-            ),
-            ListTile(
-              trailing: Icon(Icons.school),
-              title: Text("Study Mode"),
-              onTap: () {
-                Navigator.pushNamed(context, '/studyModePage');
-              },
-            ),
-            const Divider(
-              height: 1,
-              color: lightBeige,
-            ),
-            ListTile(
-              trailing: Icon(Icons.alarm),
-              title: Text("App Limits"),
-              onTap: () {
-                Navigator.pushNamed(context, '/appLimitsPage');
+              onPressed: () async {
+                await Navigator.pushNamed(context, "/profileSettings");
+                // Reload the user in case anything changed
+                await auth.currentUser?.reload();
+                // Reload UI in case things changed
+                setState(() {});
               },
             )
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              flex: 13,
-              //Container holding graph in top portion of screen
-              child: Scaffold(
-                body: [
-                  //Daily Graph
-                  Container(
-                    child: DailyGraphView(
-                        onFilteredData: updateFilteredDayData,
-                        onBarSelected: updateSelectedBar),
-                  ),
-                  //Weekly Graph
-                  Container(
-                    child: WeeklyGraphView(
-                        onFilteredData: updateFilteredWeekData,
-                        onBarSelected: updateSelectedBar),
-                  ),
-                ][graphIndex],
-                bottomNavigationBar: Container(
-                  height: 72,
-                  child: Column(
-                    children: [
-                      Container(
-                        color: beige,
-                        height: 2,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Card(
-                            color: graphIndex == 0 ? beige : null,
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10.0))),
-                            child: InkWell(
-                              child: Padding(
-                                padding: EdgeInsets.all(5.0),
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      Icons.calendar_today_rounded,
-                                      color: graphIndex == 0
-                                          ? lightBlue
-                                          : lightBeige,
-                                      size: screenHeight * 0.035,
-                                    ),
-                                    Text("  Daily  ",
-                                        style: TextStyle(
-                                            color: graphIndex == 0
-                                                ? darkBlue
-                                                : null)),
-                                  ],
-                                ),
-                              ),
-                              onTap: () {
-                                setState(() {
-                                  selectedBar = "null";
-                                  graphIndex = 0;
-                                });
-                              },
-                            ),
-                          ),
-                          Card(
-                            color: graphIndex == 1 ? beige : null,
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10.0))),
-                            child: InkWell(
-                              child: Padding(
-                                padding: EdgeInsets.all(5.0),
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      Icons.calendar_view_week_rounded,
-                                      color: graphIndex == 1
-                                          ? lightBlue
-                                          : lightBeige,
-                                      size: screenHeight * 0.035,
-                                    ),
-                                    Text("  Weekly  ",
-                                        style: TextStyle(
-                                            color: graphIndex == 1
-                                                ? darkBlue
-                                                : null)),
-                                  ],
-                                ),
-                              ),
-                              onTap: () {
-                                setState(() {
-                                  selectedBar = "null";
-                                  graphIndex = 1;
-                                });
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        color: beige,
-                        height: 2,
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-          Expanded(
-            flex: 7,
-            //Container holding list view in bottom portion of screen
-            child: ExpandedListView(
-                dayFilteredData: dayData,
-                weekFilteredData: weekData,
-                selectedBar: selectedBar,
-                appColors: appNameToColor,
-                graphIndex: graphIndex),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.all(4.0),
+            children: <Widget>[
+              SizedBox(
+                height: screenHeight * .15,
+                child: DrawerHeader(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "ProcrastiTools",
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: Image.asset("assets/logo.jpg"),
+                    ),
+                  ],
+                )),
+              ),
+              ListTile(
+                trailing: Icon(Icons.calendar_today),
+                title: Text("Calendar"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/calendarPage');
+                },
+              ),
+              const Divider(
+                height: 1,
+                color: lightBeige,
+              ),
+              ListTile(
+                trailing: Icon(Icons.school),
+                title: Text("Study Mode"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/studyModePage');
+                },
+              ),
+              const Divider(
+                height: 1,
+                color: lightBeige,
+              ),
+              ListTile(
+                trailing: Icon(Icons.alarm),
+                title: Text("App Limits"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/appLimitsPage');
+                },
+              )
+            ],
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Center(
-              child: SmoothPageIndicator(
-                controller: PageController(initialPage: 1), // Dummy controller
-                count: 3,
-                effect: WormEffect(
-                  paintStyle: PaintingStyle.stroke,
-                  activeDotColor: beige,
-                  dotColor: lightBeige,
-                  dotHeight: 8,
-                  dotWidth: 8,
-                  spacing: 12,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                flex: 13,
+                //Container holding graph in top portion of screen
+                child: Scaffold(
+                  body: [
+                    //Daily Graph
+                    Container(
+                      child: DailyGraphView(
+                          onFilteredData: updateFilteredDayData,
+                          onBarSelected: updateSelectedBar),
+                    ),
+                    //Weekly Graph
+                    Container(
+                      child: WeeklyGraphView(
+                          onFilteredData: updateFilteredWeekData,
+                          onBarSelected: updateSelectedBar),
+                    ),
+                  ][graphIndex],
+                  bottomNavigationBar: Container(
+                    height: 72,
+                    child: Column(
+                      children: [
+                        Container(
+                          color: beige,
+                          height: 2,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Card(
+                              color: graphIndex == 0 ? beige : null,
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0))),
+                              child: InkWell(
+                                child: Padding(
+                                  padding: EdgeInsets.all(5.0),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.calendar_today_rounded,
+                                        color: graphIndex == 0
+                                            ? lightBlue
+                                            : lightBeige,
+                                        size: screenHeight * 0.035,
+                                      ),
+                                      Text("  Daily  ",
+                                          style: TextStyle(
+                                              color: graphIndex == 0
+                                                  ? darkBlue
+                                                  : null)),
+                                    ],
+                                  ),
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    selectedBar = "null";
+                                    graphIndex = 0;
+                                  });
+                                },
+                              ),
+                            ),
+                            Card(
+                              color: graphIndex == 1 ? beige : null,
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0))),
+                              child: InkWell(
+                                child: Padding(
+                                  padding: EdgeInsets.all(5.0),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.calendar_view_week_rounded,
+                                        color: graphIndex == 1
+                                            ? lightBlue
+                                            : lightBeige,
+                                        size: screenHeight * 0.035,
+                                      ),
+                                      Text("  Weekly  ",
+                                          style: TextStyle(
+                                              color: graphIndex == 1
+                                                  ? darkBlue
+                                                  : null)),
+                                    ],
+                                  ),
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    selectedBar = "null";
+                                    graphIndex = 1;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          color: beige,
+                          height: 2,
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+            Expanded(
+              flex: 7,
+              //Container holding list view in bottom portion of screen
+              child: ExpandedListView(
+                  dayFilteredData: dayData,
+                  weekFilteredData: weekData,
+                  selectedBar: selectedBar,
+                  appColors: appNameToColor,
+                  graphIndex: graphIndex),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Center(
+                child: SmoothPageIndicator(
+                  controller:
+                      PageController(initialPage: 1), // Dummy controller
+                  count: 3,
+                  effect: WormEffect(
+                    paintStyle: PaintingStyle.stroke,
+                    activeDotColor: beige,
+                    dotColor: lightBeige,
+                    dotHeight: 8,
+                    dotWidth: 8,
+                    spacing: 12,
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(height: 16),
-        ],
+            Container(height: 16),
+          ],
+        ),
       ),
     );
   }
